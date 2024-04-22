de2a991659c0e2bffb88861f0d9496778caf385f
Mark Lee <mark.lee22@imperial.ac.uk>
2024-04-22 21:34:36 +0100

    this doesn't work

--- groups/__init__.py
+++ groups/__init__.py
@@ -1,3 +1,4 @@
 from .groups import CyclicGroup # noqa F401
 from .groups import GeneralLinearGroup # noqa F401
-from .shapes import Rectangle # noqa F401
+from .shapes import Square # noqa F401
+from .shapes import ShapesValueError # noqa F401
--- groups/shapes.py
+++ groups/shapes.py
@@ -1,5 +1,10 @@
+class ShapesValueError(ValueError):
+    pass
+
 class Rectangle:
     def __init__(self, length, width):
+        if length < 0 or width < 0:
+            raise ShapesValueError("dimensions cannot be negative")
         self.length = length
         self.width = width
 
@@ -8,3 +13,10 @@ class Rectangle:
 
     def __repr__(self):
         return f"{self.__class__.__name__}{self.length, self.width!r}"
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length) # Calls the __init__ function of the parent class
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.length!r})"
